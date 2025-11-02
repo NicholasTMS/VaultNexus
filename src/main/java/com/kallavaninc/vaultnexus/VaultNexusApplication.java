52a1f6f95aea52084aadb15f6465906c8cbfbb28
NicholasThongMengShui <nicholasthongmengshui@gmail.com>
2025-11-02 23:36:18 +0800

    Initialisation & gitIgnored application.properties

---
+++ src/main/java/com/kallavaninc/vaultnexus/VaultNexusApplication.java
@@ -0,0 +1,13 @@
+package com.kallavaninc.vaultnexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultNexusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaultNexusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kallavaninc/vaultnexus/VaultNexusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kallavaninc.vaultnexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaultNexusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
